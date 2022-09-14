4ec6db04ea835ff7e1766cbc1b49f3ffdc3bba09
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-09-14 20:24:52 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { StyledApp } from './styles/app.styled';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <StyledApp className="App">
+      <h1 className="hh1">TrackXp</h1>
+    </StyledApp>
   );
 }
 
---
+++ src/styles/app.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledApp = styled.div`
+
+    .hh1{
+        text-align: center;
+        width: 20vw;
+        height: 10rem;
+        margin: 0 auto;
+        background: red;
+    }
+`
