dc7b6fc6c65dfea29d9955dbe6c486f9fac5c678
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-11-08 00:25:28 +0800

    Updated

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -6,13 +6,41 @@ const AddModal = ({handleAddModal}) => {
     <StyledAddModal >
         <div className="closer" onClick={()=>handleAddModal()}/>
         <div className="add__modal">
-            <div className="add__modal__header">
-                <h1 className="title">New Order</h1>
-                <i onClick={()=>handleAddModal()} className='bx bx-collapse-alt'></i>
+          <div className="add__modal__header">
+              <h1 className="title">New Order</h1>
+              <i onClick={()=>handleAddModal()} className='bx bx-collapse-alt'></i>
+          </div>
+          <form className="add__modal__body">
+
+            <div className="add__user__info">
+              <h2>Costumer Info</h2>
+              <div className="fullName__input__container">
+                <div className="user__input">
+                  <p>First Name</p>
+                  <input type="text" placeholder="Enter First Name"/>
+                </div>
+                <div className="user__input">
+                  <p>Last Name</p>
+                  <input type="text" placeholder="Enter Last Name"/>
+                </div>
+              </div>
+              <div className="contact__container">
+                <p>Contact No.</p>
+                <input type="number" placeholder="Enter Contact No."/>
+              </div>
+              <div className="payment__container">
+                <p>Payment</p>
+                <select name="payment" id="payment">
+                  <option value="Pending">Pending</option>
+                  <option value="Paid">Paid</option>
+                </select>
+              </div>
             </div>
-            <form className="add__modal__body">
-              
-            </form>
+
+            <div className="add__user__particulars">
+
+            </div>
+          </form>
         </div>
     </StyledAddModal>
   )
--- src/styles/AddModal.styled.js
+++ src/styles/AddModal.styled.js
@@ -1,9 +1,19 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const StyledAddModal = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 110;
+
+  .closer {
     position: fixed;
     top: 0;
     right: 0;
@@ -11,59 +21,128 @@ export const StyledAddModal = styled.div`
     bottom: 0;
     width: 100vw;
     height: 100vh;
-    z-index: 110;
-    
-    .closer {
-        position: fixed;
-        top: 0;
-        right: 0;
-        left: 0;
-        bottom: 0;
-        width: 100vw;
-        height: 100vh;
-        background-color: rgba(0, 0, 0, 0.5);
-        backdrop-filter: blur(5px);
-        z-index: 101;
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(5px);
+    z-index: 101;
+  }
+
+  .add__modal {
+    display: flex;
+    flex-direction: column;
+    width: 95%;
+    height: 90%;
+    background: #fafafa;
+    border-radius: 1rem;
+    padding: 1rem;
+    z-index: 111;
+
+    .add__modal__header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      padding: 0.5rem 0;
+
+      .title {
+        font-size: 1.5rem;
+      }
+
+      i {
+        cursor: pointer;
+        font-size: 1.5rem;
+        padding: 0.3rem;
+      }
     }
-    .add__modal {
-        width: 95%;
-        height: 90%;
-        background: white;
-        border-radius: 1rem;
+
+    .add__modal__body {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 100%;
+      overflow-y: scroll;
+
+      .add__user__info {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        border: 1px solid #ccc;
         padding: 1rem;
-        z-index: 111;
+        border-radius: 1rem;
+        background: white;
 
-        .add__modal__header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
+        h2 {
+          font-size: 1.3rem;
+          font-weight: 500;
+          padding: 0.3rem 0;
+        }
+
+        .fullName__input__container {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+
+          .user__input {
+            width: 48%;
+            p {
+              font-size: 0.8rem;
+              margin-left: 0.7rem;
+            }
+            input {
+              width: 100%;
+              border: none;
+              border-radius: 0.5rem;
+              padding: 0.8rem;
+              border: 1px solid #ccc;
+            }
+          }
+        }
+
+        .contact__container {
+          width: 100%;
+          p {
+            font-size: 0.8rem;
+            margin-left: 0.7rem;
+          }
+          input {
             width: 100%;
-            
-            .title {
-                font-size: 1.5rem;
-            }
-            
-            i {
-                cursor: pointer;
-                font-size: 1.5rem;
-                padding: .3rem;
-            }
+            border: none;
+            border-radius: 0.5rem;
+            padding: 0.8rem;
+            border: 1px solid #ccc;
+          }
         }
-    }
+        .payment__container {
+          display: flex;
+          width: 100%;
+          justify-content: space-between;
+          align-items: center;
+          gap: 1rem;
 
-
-    @media screen and (min-width: 768px) {
-        .add__modal {
-            width: 80%;
-            height: 80%;
-            background: white;
-            border-radius: 1rem;
-            padding: 2rem;
+          select {
+            text-align: center;
+            width: 100%;
+            border: none;
+            border-radius: 0.5rem;
+            padding: 0.5rem;
+            border: 1px solid #ccc;
+          }
         }
+      }
     }
+  }
 
-    @media screen and (min-width: 1024px) {
-        .add__modal {
-        }
+  @media screen and (min-width: 768px) {
+    .add__modal {
+      width: 85%;
+      height: 90%;
+      background: white;
+      border-radius: 1rem;
+      padding: 1rem;
     }
-`
+  }
+
+  @media screen and (min-width: 1024px) {
+    .add__modal {
+    }
+  }
+`;
