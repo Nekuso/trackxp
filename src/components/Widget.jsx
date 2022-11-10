1b285bb6a67b8903fbab50f08921d309df63029a
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-11-11 07:49:10 +0800

    Update Widget.jsx

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { StyledWidget } from '../styles/Widget.styled';
-import {collection, query, where, getDocs, onSnapshot } from "firebase/firestore";
+import {collection, query, where, getDocs,} from "firebase/firestore";
 import { db } from "../firebase";
 
 
@@ -57,6 +57,27 @@ const Widget = ({type}) => {
     break;
   }
 
+  const handleEarnings = () => {
+    let totalEarnings = 0;
+      let totalEarningsDiff = 0;
+      earningsData.forEach((item) => {
+        if (item.payment === "Paid") {
+          totalEarnings += item.total;
+        }
+      }
+      );
+      earningsDataDiff.forEach((item) => {
+        if (item.payment === "Paid") {
+          totalEarningsDiff += item.total;
+        }
+      }
+      );
+      setEarnings(totalEarnings);
+      setEarningsDiff(
+        ((totalEarnings - totalEarningsDiff) / totalEarningsDiff) * 100
+      );
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const today = new Date();
@@ -76,6 +97,7 @@ const Widget = ({type}) => {
 
       const lastMonthData = await getDocs(lastMonthQuery);
       const prevMonthData = await getDocs(prevMonthQuery);
+
       
       setAmount(lastMonthData.docs.length);
       setDiff(
@@ -83,85 +105,83 @@ const Widget = ({type}) => {
           100
       );
 
-      let list1 = []
-
-      lastMonthData.forEach((doc) => {
-        list1.push({ id: doc.id, ...doc.data() })
+      let list = [];
+      lastMonthData.docs.forEach((doc) => {
+        list.push({ id: doc.id, ...doc.data() });
       });
-      setEarningsData(list1);
-      
+      setEarningsData(list);
 
+      let list2 = [];
+      prevMonthData.docs.forEach((doc) => {
+        list2.push({ id: doc.id, ...doc.data() });
+      });
+      setEarningsDataDiff(list2);
+
+        //     const lastEarnings = query(
+        //     collection(db, "orders"),
+        //     where("timeStamp", "<=", today), 
+        //     where("timeStamp", ">", lastMonth)
+        //   );
+        
+        //   const prevEarnings = query(
+        //     collection(db, "orders"),
+        //     where("timeStamp", "<=", lastMonth),
+        //     where("timeStamp", ">", prevMonth) 
+        //   );
+        
+        //   const unsub = onSnapshot(lastEarnings,(snapshot) => {
+        //     let list = [];
+        //     snapshot.docs.forEach((doc) => {
+        //       list.push({ id: doc.id, ...doc.data() });
+        //     });
+        //     setEarningsData(list);
+        //   });
+        
+        //   const unsub2 = onSnapshot(prevEarnings,(snapshot) => {
+        //     let list = [];
+        //     snapshot.docs.forEach((doc) => {
+        //       list.push({ id: doc.id, ...doc.data() });
+        //     });
+        //     setEarningsDataDiff(list);
+        //   });
+        
+        //   let totalEarnings = 0;
+        //   let totalEarningsDiff = 0;
+        //   earningsData.forEach((item) => {
+        //     if (item.payment === "Paid") {
+        //       totalEarnings += item.total;
+        //     }
+        //   }
+        //   );
+        //   earningsDataDiff.forEach((item) => {
+        //     if (item.payment === "Paid") {
+        //       totalEarningsDiff += item.total;
+        //     }
+        //   }
+        //   );
+        //   setEarnings(totalEarnings);
+        //   setEarningsDiff(
+        //     ((totalEarnings - totalEarningsDiff) / totalEarningsDiff) * 100
+        //   );
+      
+        //   return () => {
+        //     unsub();
+        //     unsub2();
+        //   }
 
     };
     fetchData();
-
-  }, []);
-  
-  // useEffect(() => {
-  //   const today = new Date();
-  //   const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
-  //   const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
-
-  //   const lastEarnings = query(
-  //     collection(db, "orders"),
-  //     where("timeStamp", "<=", today), 
-  //     where("timeStamp", ">", lastMonth)
-  //   );
-  
-  //   const prevEarnings = query(
-  //     collection(db, "orders"),
-  //     where("timeStamp", "<=", lastMonth),
-  //     where("timeStamp", ">", prevMonth) 
-  //   );
-  
-  //   const unsub = onSnapshot(lastEarnings,(snapshot) => {
-  //     let list = [];
-  //     snapshot.docs.forEach((doc) => {
-  //       list.push({ id: doc.id, ...doc.data() });
-  //     });
-  //     setEarningsData(list);
-  //   });
-  
-  //   const unsub2 =onSnapshot(prevEarnings,(snapshot) => {
-  //     let list = [];
-  //     snapshot.docs.forEach((doc) => {
-  //       list.push({ id: doc.id, ...doc.data() });
-  //     });
-  //     setEarningsDataDiff(list);
-  //   });
-  
-  //   let totalEarnings = 0;
-  //   let totalEarningsDiff = 0;
-  //   earningsData.forEach((item) => {
-  //     if (item.payment === "Paid") {
-  //       totalEarnings += item.total;
-  //     }
-  //   }
-  //   );
-  //   earningsDataDiff.forEach((item) => {
-  //     if (item.payment === "Paid") {
-  //       totalEarningsDiff += item.total;
-  //     }
-  //   }
-  //   );
-  //   setEarnings(totalEarnings);
-  //   setEarningsDiff(
-  //     ((totalEarnings - totalEarningsDiff) / totalEarningsDiff) * 100
-  //   );
-
-  //   return () => {
-  //     unsub();
-  //     unsub2();
-  //   }
-  // })
-
+    handleEarnings();
+  }, [amount]);
 
   return (
     <StyledWidget>
         <div className="top">
           <h3 className="top__title">{data.title}</h3>
-          <div className={`percentage ${diff < 0 ? "negative" : "positive"}`}>
-            {diff < 0 ? <i className='bx bxs-down-arrow'></i> : <i className='bx bxs-up-arrow'></i> }
+          <div className={`percentage ${data.query === "orders" && (diff < 0 ? "negative" : "positive")} ${data.query === "earnings" && ( earningsDiff < 0 ? "negative" : "positive")}`}>
+            {data.query === "orders" && (diff < 0 ? <i className='bx bxs-down-arrow'></i> : <i className='bx bxs-up-arrow'></i>)}
+            {data.query === "earnings" && (earningsDiff < 0 ? (<i className='bx bxs-down-arrow'></i>) : (<i className='bx bxs-up-arrow'></i>))}
+            {/* {data.query === "earnings" && earningsDiff !== 0 ? <i className='bx bxs-down-arrow'></i> : <i className='bx bxs-up-arrow'></i>}} */}
             <p>
               {data.query === "orders" ? diff : earningsDiff} %
             </p>
