14ed1300c82290b85583a1aae2e42a6356e9058f
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-01-04 23:07:07 +0800

    Updated

--- src/components/SingleOrder.jsx
+++ src/components/SingleOrder.jsx
@@ -1,247 +1,287 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { StyledSinglePage } from '../styles/SinglePage.styled';
-import { useParams } from 'react-router-dom';
-import { collection, onSnapshot, query, where, getDocs } from 'firebase/firestore';
-import { db } from '../firebase';
-import QRCode from 'qrcode';
-import { EditModal } from './EditModal';
-import UpdateButton from './UpdateButton';
-import {motion, AnimatePresence} from "framer-motion";
-import CountUp from 'react-countup';
-import { useReactToPrint } from 'react-to-print';
-import { StyledPrintOrderInfo } from '../styles/printOrderInfo';
+import React, { useEffect, useState, useRef } from "react";
+import { StyledSinglePage } from "../styles/SinglePage.styled";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  onSnapshot,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import QRCode from "qrcode";
+import { EditModal } from "./EditModal";
+import UpdateButton from "./UpdateButton";
+import { motion, AnimatePresence } from "framer-motion";
+import CountUp from "react-countup";
+import { useReactToPrint } from "react-to-print";
+import { StyledPrintOrderInfo, StyledPrint } from "../styles/printOrderInfo";
 
-const SingleOrder = ({handleUpdateNotifcation, handleCycleNotification}) => {
+const SingleOrder = ({ handleUpdateNotifcation, handleCycleNotification }) => {
+  const [order, setOrder] = useState([]);
+  const [queryOrder, setQueryOrder] = useState([]);
+  const { orderId } = useParams();
+  const [docId, setDocId] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const qrLink = `https://nekuso.github.io/trackxp/?#/${orderId}`;
+  const [isEditModal, setIsEditModal] = useState(false);
+  const [cycleCollectionCount, setCycleCollectionCount] = useState(0);
 
-    const [order, setOrder] = useState([]);
-    const [queryOrder, setQueryOrder] = useState([]);
-    const { orderId } = useParams();
-    const [docId, setDocId] = useState("");
-    const [qrCode, setQrCode] = useState("");
-    const qrLink =`https://nekuso.github.io/trackxp/?#/${orderId}`;
-    const [isEditModal, setIsEditModal] = useState(false);
-    const [cycleCollectionCount, setCycleCollectionCount] = useState(0);
+  let componentRef = useRef();
 
-    let componentRef = useRef();
-    
-    const handleEditModal = () => {
-        setIsEditModal(!isEditModal);
-    }
+  const handleEditModal = () => {
+    setIsEditModal(!isEditModal);
+  };
 
-    useEffect(() => {
-        const unsub = onSnapshot(
-            collection(db, "orders"),
-            (snapShot) => {
-              let item = [];
-              snapShot.docs.forEach((doc) => {
-                item.push({ id: doc.id, ...doc.data() });
-              });
-              setQueryOrder(item);
-            },
-            (error) => {
-              console.log(error);
-            }
-        );
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, "orders"),
+      (snapShot) => {
+        let item = [];
+        snapShot.docs.forEach((doc) => {
+          item.push({ id: doc.id, ...doc.data() });
+        });
+        setQueryOrder(item);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
-        return () => {
-            unsub();
-        };
-    }, []);
+    return () => {
+      unsub();
+    };
+  }, []);
 
-    // Generates QR Code
-    const GenerateQRCode = () => {
-        QRCode.toDataURL(qrLink, (err, url) => {
-            if (err) throw err
-            setQrCode(url);
-        })
-    }
-    
-    useEffect(() => {
-        //get order by orders id
-        const fetchOrder = async () => {
-            const orderRef = query( collection(db, "orders"), where("orderId", "==", parseInt(orderId)));
-            const orderSnap = await getDocs(orderRef);
-            orderSnap.forEach((doc) => {
-                setOrder(doc.data());
-                setDocId(doc.id);
-                setCycleCollectionCount(doc.data().cycleStatusCollection.length);
-            })
-        }
+  // Generates QR Code
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(qrLink, (err, url) => {
+      if (err) throw err;
+      setQrCode(url);
+    });
+  };
 
-        return () => {
-            fetchOrder();
-            GenerateQRCode();
-        };
-    }, [orderId, queryOrder]);
+  useEffect(() => {
+    //get order by orders id
+    const fetchOrder = async () => {
+      const orderRef = query(
+        collection(db, "orders"),
+        where("orderId", "==", parseInt(orderId))
+      );
+      const orderSnap = await getDocs(orderRef);
+      orderSnap.forEach((doc) => {
+        setOrder(doc.data());
+        setDocId(doc.id);
+        setCycleCollectionCount(doc.data().cycleStatusCollection.length);
+      });
+    };
 
+    return () => {
+      fetchOrder();
+      GenerateQRCode();
+    };
+  }, [orderId, queryOrder]);
 
-    const container = {
-        hiddenV: { opacity: 0, y: 20 },
-        visible: {
-            opacity: 1,
-            y:0,
-            transition: {
-                duration: 5,
-                type: "spring",
-                stiffness: 90,
-                when: "beforeChildren",
-                staggerChildren: 0.3,
-            }
-        },
-    }
+  const container = {
+    hiddenV: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 5,
+        type: "spring",
+        stiffness: 90,
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+  };
 
-    const viewVariants = {
-        initialHidden: {
-          opacity: 0,
-        },
-        initialVisible: {
-          opacity: 1,
-          transition: {
-            delay: 0.5,
-            duration: .6,
-          }
-        },
-        hiddenV: { opacity: 0, y: 20 },
-        visible: {
-            opacity: 1,
-            y:0,
-        },
-    }
-    
-    const handlePrint = useReactToPrint({
-        content: () => componentRef.current,
-        copyStyles: true,
-        pageStyle: `
+  const viewVariants = {
+    initialHidden: {
+      opacity: 0,
+    },
+    initialVisible: {
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+        duration: 0.6,
+      },
+    },
+    hiddenV: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+    copyStyles: true,
+    pageStyle: `
             @page {
                 size: Roll Paper 80 x 297mm;
                 margin: auto 20mm;
             }
         `,
-    });
+  });
 
-    return (
-        <StyledSinglePage>
-            <motion.div className="single__page__section"
-                variants={viewVariants}
-                initial="initialHidden"
-                animate="initialVisible"
-                exit="initialHidden"
+  return (
+    <StyledSinglePage>
+      <motion.div
+        className="single__page__section"
+        variants={viewVariants}
+        initial="initialHidden"
+        animate="initialVisible"
+        exit="initialHidden"
+      >
+        <AnimatePresence>
+          {isEditModal ? (
+            <EditModal
+              handleEditModal={handleEditModal}
+              order={order}
+              docId={docId}
+              handleUpdateNotifcation={handleUpdateNotifcation}
+            />
+          ) : null}
+        </AnimatePresence>
+        <div className="update__controls">
+          <h2 className="title">Order Status</h2>
+          <div className="buttons">
+            <UpdateButton
+              order={order}
+              docId={docId}
+              cycleCollectionCount={cycleCollectionCount}
+              handleCycleNotification={handleCycleNotification}
+            />
+            <div className="button" onClick={handleEditModal}>
+              <i className="bx bxs-edit"></i>
+              <p>Edit Order</p>
+            </div>
+            <div className="button" onClick={handlePrint}>
+              <i className="bx bxs-printer"></i>
+              <p>Print Receipt</p>
+            </div>
+          </div>
+        </div>
+        <motion.div
+          className="order__cycle__container"
+          variants={container}
+          initial="hiddenV"
+          animate="visible"
+          whileHover={{ scale: 1.05 }}
+        >
+          {order.cycleStatusCollection ? (
+            order.cycleStatusCollection.map((item, index) => (
+              <motion.div
+                className="order__cycle__item"
+                key={index}
+                variants={container}
+                initial="hiddenV"
+                layout
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: 0.5 + index * 0.2,
+                    duration: 5,
+                    type: "spring",
+                    stiffness: 90,
+                    when: "beforeChildren",
+                    staggerChildren: 0.3,
+                  },
+                }}
+              >
+                <i className={`bx ${item.icon}`}></i>
+                <div className="order__cycle__title">
+                  <p className="cycle__title">{item.name}</p>
+                  <p className="cycle__stamp">{item.timeStamp}</p>
+                </div>
+              </motion.div>
+            ))
+          ) : (
+            <div
+              style={{ textAlign: "center", width: "100%", fontWeight: "bold" }}
             >
-                <AnimatePresence>
-                    {isEditModal ? <EditModal handleEditModal={handleEditModal} order={order} docId={docId} handleUpdateNotifcation={handleUpdateNotifcation}/> : null}
-                </AnimatePresence>
-                <div className="update__controls">
-                    <h2 className="title">Order Status</h2>
-                    <div className="buttons">
-                        <UpdateButton order={order} docId={docId} cycleCollectionCount={cycleCollectionCount} handleCycleNotification={handleCycleNotification}/>
-                        <div className="button" onClick={handleEditModal}>
-                            <i className='bx bxs-edit' ></i>
-                            <p>Edit Order</p>
-                        </div>
-                        <div className="button" onClick={handlePrint}>
-                            <i className='bx bxs-printer'></i>
-                            <p>Print Receipt</p>
-                        </div>
-                    </div>
+              Loading
+            </div>
+          )}
+        </motion.div>
+        <StyledPrintOrderInfo className="order__info__container" ref={componentRef}>
+          <div className="order__info">
+            <img src={qrCode} alt="qrcode" />
+            <div className="order__title">
+              <h2 className="order__id">ORDER ID: {order.orderId}</h2>
+              <p href={qrLink}>nekuso.github.io/trackxp/?#/{order.orderId}</p>
+            </div>
+            <div className="order__desc">
+              <div className="left">
+                <div className="item__desc">
+                  <p className="item__title">Order Date</p>
+                  <p>{order.dateCreated}</p>
                 </div>
-                <motion.div className="order__cycle__container"
-                    variants={container}
-                    initial="hiddenV"
-                    animate="visible"
-                    whileHover={{ scale: 1.05 }}
-                    >
-                        {order.cycleStatusCollection ? order.cycleStatusCollection.map((item, index) => (
-                            <motion.div className="order__cycle__item" key={index}
-                                variants={container}
-                                initial="hiddenV"
-                                layout
-                                animate={{
-                                    opacity: 1,
-                                    y:0,
-                                    transition: {
-                                        delay: 0.5 + index * 0.2,
-                                        duration: 5,
-                                        type: "spring",
-                                        stiffness: 90,
-                                        when: "beforeChildren",
-                                        staggerChildren: 0.3,
-                                    }
-                                }}
-                                
-                            >
-                                <i className={`bx ${item.icon}`}></i>
-                                <div className="order__cycle__title">
-                                    <p className="cycle__title">{item.name}</p>
-                                    <p className="cycle__stamp">{item.timeStamp}</p>
-                                </div>
-                            </motion.div>
-                        )): <div style={{textAlign: 'center', width: '100%', fontWeight: "bold"}}>Loading</div>}
-                </motion.div>
-                <StyledPrintOrderInfo className="order__info__container" ref={componentRef} >
-                    <div className="order__info">
-                        <img src={qrCode} alt="qrcode" />
-                        <div className="order__title">
-                            <h2 className="order__id">ORDER ID: {order.orderId}</h2>
-                            <p href={qrLink}>nekuso.github.io/trackxp/?#/{order.orderId}</p>
-                        </div>
-                        <div className="order__desc">
-                            <div className="left">
-                                <div className="item__desc">
-                                    <p className="item__title">Order Date</p>
-                                    <p>{order.dateCreated}</p>
-                                </div>
-                                <div className="item__desc">
-                                    <p className="item__title">Name</p>
-                                    <p>{order.firstName + " " + order.lastName}</p>
-                                </div>
-                                <div className="item__desc">
-                                    <p className="item__title">Payment</p>
-                                    <p>{order.payment}</p>
-                                </div>
-                            </div>
-                            <div className="right">
-                                <div className="item__desc">
-                                    <p className="item__title">Contact No.</p>
-                                    <p>{order.contact}</p>
-                                </div>
-                                <div className="item__desc">
-                                    <p className="item__title">Price</p>
-                                    <p>P {order.total}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </StyledPrintOrderInfo>
-                <div className="order__table__container" >
-                    <table className="order__table">
-                        <thead>
-                            <tr className="header">
-                                <th className="particular__align">Particulars</th>
-                                <th>Quantity</th>
-                                <th>Price</th>
-                                <th>Total</th>
-                            </tr>   
-                        </thead>
-                        <tbody>
-                            {order.particularsData ? order.particularsData.map((item, index) => (
-                                <tr key={index}>
-                                    <td className="particular__align">{item.name}</td>
-                                    <td>{item.quantity}</td>
-                                    <td>{item.price}</td>
-                                    <td>{item.itemTotal}</td>
-                                </tr>
-                            )) : <tr style={{textAlign: 'center', width: '100%'}}><td>{"Loading"}</td></tr>}
+                <div className="item__desc">
+                  <p className="item__title">Name</p>
+                  <p>{order.firstName + " " + order.lastName}</p>
+                </div>
+                <div className="item__desc">
+                  <p className="item__title">Payment</p>
+                  <p>{order.payment}</p>
+                </div>
+              </div>
+              <div className="right">
+                <div className="item__desc">
+                  <p className="item__title">Contact No.</p>
+                  <p>{order.contact}</p>
+                </div>
+                <div className="item__desc">
+                  <p className="item__title">Price</p>
+                  <p>P {order.total}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </StyledPrintOrderInfo>
+        <div className="order__table__container">
+          <table className="order__table">
+            <thead>
+              <tr className="header">
+                <th className="particular__align">Particulars</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.particularsData ? (
+                order.particularsData.map((item, index) => (
+                  <tr key={index}>
+                    <td className="particular__align">{item.name}</td>
+                    <td>{item.quantity}</td>
+                    <td>{item.price}</td>
+                    <td>{item.itemTotal}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr style={{ textAlign: "center", width: "100%" }}>
+                  <td>{"Loading"}</td>
+                </tr>
+              )}
 
-                            <tr>
-                                <td className="particular__align grand__total" colSpan="3">Grand Total</td>
-                                <td className="grand__total"><CountUp duration={1} end={order.total}/></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </motion.div>
-        </StyledSinglePage>
-    );
-}
+              <tr>
+                <td className="particular__align grand__total" colSpan="3">
+                  Grand Total
+                </td>
+                <td className="grand__total">
+                  <CountUp duration={1} end={order.total} />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </motion.div>
+    </StyledSinglePage>
+  );
+};
 
 export default SingleOrder;
--- src/styles/printOrderInfo.js
+++ src/styles/printOrderInfo.js
@@ -1,155 +1,222 @@
 import styled from "styled-components";
 
 export const StyledPrintOrderInfo = styled.div`
+  width: 100%;
+  .order__info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     width: 100%;
-      .order__info {
+    padding: 1.3rem;
+    border-radius: 0.7rem;
+    border: 1px solid #e0e0e0;
+    transition: all 0.3s cubic-bezier(0.47, 0, 0.37, 1.47);
+    background: #ffffff69;
+    backdrop-filter: blur(10px);
+    cursor: pointer;
+
+    :hover {
+      border: 1px solid black;
+      box-shadow: black 10px 10px 0px;
+      transform: translate(-5px, -5px);
+    }
+
+    img {
+      width: 90%;
+      border-radius: 0.7rem;
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+
+    .order__title {
+      padding: 1rem 0;
+      width: 100%;
+      text-align: center;
+
+      h2 {
+        font-size: 5vw;
+      }
+
+      p {
+        font-size: 4vw;
+        color: black;
+      }
+    }
+
+    .order__desc {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      padding-top: 1rem;
+
+      .left,
+      .right {
         display: flex;
-        justify-content: center;
-        align-items: center;
         flex-direction: column;
-        width: 100%;
-        padding: 1.3rem;
-        border-radius: 0.7rem;
-        border: 1px solid #e0e0e0;
-        transition: all 0.3s cubic-bezier(0.47, 0, 0.37, 1.47);
-        background: #ffffff69;
-        backdrop-filter: blur(10px);
-        cursor: pointer;
-        
-        :hover {
-          border: 1px solid black;
-          box-shadow: black 10px 10px 0px;
-          transform: translate(-5px, -5px);
-        }
-
-        img {
-          width: 90%;
-          border-radius: 0.7rem;
-          box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-        }
-
-        .order__title {
-          padding: 1rem 0;
-          width: 100%;
-          text-align: center;
-
-          h2 {
-            font-size: 5vw;
-          }
-
+        row-gap: 0.7rem;
+        .item__desc {
           p {
+            font-weight: 400;
             font-size: 4vw;
-            color: black;
+          }
+          .item__title {
+            font-weight: 700;
+            font-size: 4vw;
           }
         }
+      }
+    }
+  }
 
-        .order__desc {
+  @media screen and (min-width: 768px) {
+    flex: 1 0 15%;
+
+    .order__info {
+      width: 100%;
+      height: auto;
+      padding: 1rem;
+
+      img {
+        width: 160px;
+      }
+
+      .order__title {
+        padding-top: 0.5rem;
+        h2 {
+          font-size: 0.7rem;
+        }
+        p {
+          font-size: 0.6rem;
+        }
+      }
+
+      .order__desc {
+        padding: 0;
+
+        .left,
+        .right {
+          row-gap: 0.4rem;
+          .item__desc {
+            text-overflow: ellipsis;
+            p {
+              font-size: 0.7rem;
+            }
+            .item__title {
+              font-size: 0.6rem;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    flex: 1 0 2%;
+    height: auto;
+
+    .order__info {
+      width: 100%;
+      height: auto;
+      padding: 1.5rem;
+
+      img {
+        width: 180px;
+      }
+
+      .order__title {
+        padding-top: 0.5rem;
+        h2 {
+          font-size: 0.7rem;
+        }
+        p {
+          font-size: 0.6rem;
+        }
+      }
+      .order__desc {
+        padding: 0;
+        .left,
+        .right {
+          row-gap: 0.2rem;
+          .item__desc {
+            text-overflow: ellipsis;
+            p {
+              font-size: 0.7rem;
+            }
+            .item__title {
+              font-size: 0.7rem;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media print {
+    width: 100%;
+    .order__info {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      width: 100%;
+      padding: 1.3rem;
+      border-radius: 0.7rem;
+      border: 1px solid #e0e0e0;
+      transition: all 0.3s cubic-bezier(0.47, 0, 0.37, 1.47);
+      background: #ffffff69;
+      backdrop-filter: blur(10px);
+      cursor: pointer;
+
+      :hover {
+        border: 1px solid black;
+        box-shadow: black 10px 10px 0px;
+        transform: translate(-5px, -5px);
+      }
+
+      img {
+        width: 90%;
+        border-radius: 0.7rem;
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+      }
+
+      .order__title {
+        padding: 1rem 0;
+        width: 100%;
+        text-align: center;
+
+        h2 {
+          font-size: 7.5vw;
+        }
+
+        p {
+          font-size: 5.5vw;
+          color: black;
+        }
+      }
+
+      .order__desc {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding-top: 1rem;
+
+        .left,
+        .right {
           display: flex;
-          justify-content: space-between;
-          width: 100%;
-          padding-top: 1rem;
-
-          .left,
-          .right {
-            display: flex;
-            flex-direction: column;
-            row-gap: 0.7rem;
-            .item__desc {
-              p {
-                font-weight: 400;
-                font-size: 4vw;
-              }
-              .item__title {
-                font-weight: 700;
-                font-size: 4vw;
-              }
-            }
-          }
-        }
-    }
-
-    @media screen and (min-width: 768px) {
-        flex: 1 0 15%;
-
-        .order__info {
-          width: 100%;
-          height: auto;
-          padding: 1rem;
-
-          img {
-            width: 160px;
-          }
-
-          .order__title {
-            padding-top: 0.5rem;
-            h2 {
-              font-size: 0.7rem;
-            }
+          flex-direction: column;
+          row-gap: 0.7rem;
+          .item__desc {
             p {
-              font-size: 0.6rem;
+              font-weight: 500;
+              font-size: 6vw;
             }
-          }
-
-          .order__desc {
-            padding: 0;
-
-            .left,
-            .right {
-              row-gap: 0.4rem;
-              .item__desc {
-                text-overflow: ellipsis;
-                p {
-                  font-size: 0.7rem;
-                }
-                .item__title {
-                  font-size: 0.6rem;
-                }
-              }
+            .item__title {
+              font-weight: 700;
+              font-size: 7.4vw;
             }
           }
         }
+      }
     }
-
-    @media screen and (min-width: 1024px) {
-        flex: 1 0 2%;
-        height: auto;
-
-        .order__info {
-          width: 100%;
-          height: auto;
-          padding: 1.5rem;
-
-          img {
-            width: 180px;
-          }
-
-          .order__title {
-            padding-top: 0.5rem;
-            h2 {
-              font-size: 0.7rem;
-            }
-            p {
-              font-size: 0.6rem;
-            }
-          }
-
-          .order__desc {
-            padding: 0;
-            .left,
-            .right {
-              row-gap: 0.2rem;
-              .item__desc {
-                text-overflow: ellipsis;
-                p {
-                  font-size: 0.7rem;
-                }
-                .item__title {
-                  font-size: 0.7rem;
-                }
-              }
-            }
-          }
-        }
-    }
-
-`
+  }
+`;
